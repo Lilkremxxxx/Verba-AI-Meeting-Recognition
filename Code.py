46a5d0798508e8466efc710240b253d42d0058a6
Lilkremxxx <bangnn@tks.edu.vn>
2026-01-28 16:46:51 +0700

    Guide how to use PGsql in python

---
+++ Code.py
@@ -0,0 +1,43 @@
+import asyncio
+import asyncpg
+from dotenv import load_dotenv
+import os
+from datetime import datetime
+import time
+
+load_dotenv()
+PG_HOST=os.getenv("PG_HOST")
+PG_PORT=os.getenv("PG_PORT")
+PG_DBNAME=os.getenv("PG_DBNAME")
+PG_USER=os.getenv("PG_USER")
+PG_PASSWORD=os.getenv("PG_PASSWORD")
+
+async def test_dtb():
+    conn = await asyncpg.connect(
+            host=PG_HOST, 
+            port=int(PG_PORT),
+            database=PG_DBNAME,
+            user=PG_USER, 
+            password=PG_PASSWORD
+        )
+    #conn.execute("CREATE EXTENSION IF NOT EXISTS vector;")
+    rows = await conn.fetch('SELECT * FROM users;')
+    print(rows)
+    print("Finish")
+
+async def add_data_dtb():
+    start_time = datetime.now()
+    conn = await asyncpg.connect(
+            host=PG_HOST, 
+            port=int(PG_PORT),
+            database=PG_DBNAME,
+            user=PG_USER, 
+            password=PG_PASSWORD
+        )
+    await conn.execute(
+        'INSERT INTO "users" ( "email", "password_hash") VALUES ($1, $2)',
+        "bangpoca@gmail.com", '1234'
+    )
+    print("Finish insert into dtb")
+asyncio.run(test_dtb())
+#asyncio.run(add_data_dtb())
