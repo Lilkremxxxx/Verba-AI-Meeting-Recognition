b2ef44e52a5c49178a9dab343379ba7c24f40056
Lilkremxxx <bangnn@tks.edu.vn>
2026-01-29 23:20:03 +0700

    Update meeting api and use corsmiddleware + middleware

--- backend/app/api/endpoints/meetings.py
+++ backend/app/api/endpoints/meetings.py
@@ -1,2 +1,25 @@
 # Meetings endpoints
+import asyncio
+from fastapi import APIRouter, File, Form, UploadFile, HTTPException
+from pydantic import BaseModel
+from typing import List
+import os
 
+router = APIRouter()
+
+
+
+@router.post("/meetings/upload")
+async def meetings_upload(files: List[UploadFile] = File(...)):
+    saved_files =[]
+    file_info =[]
+    for file in files:
+        try:
+            filename, filetail = os.path.splitext(file.filename)
+            filetail = filetail.lower().lstrip(".")
+            print(filename, filetail)
+            if filetail not in ["mp3", "wav"]:
+                raise HTTPException(status_code=422, detail=f"File {file.filename} Unsupported file format. Use: mp3, wav")
+            
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Failed to save {file.filename}: {str(e)}")
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,57 @@
 # Main application
+from enum import Enum
+import time
+import asyncio
+from fastapi import FastAPI, Header, Request
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import Annotated
+from api.endpoints.meetings import router as meetings_endpoint_router
 
-hi
+
+app = FastAPI()
+#CROs middleware
+origins =[
+    "http://localhost:8000",
+    "http://localhost:5173",
+]
+
+
+
+#Middleware
+@app.middleware("http")
+async def start_time(request: Request, call_next):
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    process_time = time.perf_counter() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
+
+
+
+app.include_router(meetings_endpoint_router)
+
+
+class Item(BaseModel):
+    name: str
+    des: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.post("/items/upload1")
+async def create_item(items: Item):
+    return items
+
+@app.get("/items/")
+async def read_items(q: Annotated[str | None, Header()] = None):
+    return {"q":q}
+    
